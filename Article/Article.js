2e273d2385a04d5b31361bfbe71cbae8e8f4255f
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-15 14:46:38 +0100

    Add article.less to index.html Add solutions to article.js

--- Article/Article.js
+++ Article/Article.js
@@ -5,17 +5,18 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.article = article;
     // create a reference to the ".expandButton" class.
-    this.expandButton = article.querySelectorAll(".expandButton");
+    this.expandButton = this.article.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.forEach(button => {
-      button.textContent = "expand";
-    });
-
+    // this.expandButton.forEach(button => {
+    //   button.textContent = "expand";
+    // });
+    this.expandButton.textContent = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener('click',() => this.expandArticle());
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.article.forEach(expand => expand.classList.toggle("article-open"));
+    this.article.classList.toggle("article-open");
   }
 }
 
