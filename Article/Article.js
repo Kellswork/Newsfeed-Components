a74d070cc3c9e1a4ccd6d33dd6d899ec73030209
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-15 13:46:57 +0100

    Add menu.less file to index.less Add solution to menu.js file

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,21 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.article = article;
+    // create a reference to the ".expandButton" class.
+    this.expandButton = article.querySelectorAll(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    this.expandButton.forEach(button => {
+      button.textContent = "expand";
+    });
 
+    // Set a click handler on the expandButton reference, calling the expandArticle method.
+  }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.article.forEach(expand => expand.classList.toggle("article-open"));
   }
 }
 
@@ -26,4 +27,7 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll(".article");
+articles.forEach(article => {
+  new Article(article);
+});
